43b43d34b2409378d019ff6e0c3770b28c25cf09
thbdias <thiagobalbino91@gmail.com>
2019-12-12 14:29:37 -0300

    salvando novo produto

--- src/dao/DaoProduto.java
+++ src/dao/DaoProduto.java
@@ -1,7 +1,10 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import beans.BeanProduto;
 import connection.SingleConnection;
 
 public class DaoProduto {
@@ -10,5 +13,25 @@ public class DaoProduto {
 	
 	public DaoProduto() {
 		connection = SingleConnection.getConnection();
+	}
+	
+	public void salvar(BeanProduto produto) {		
+		try {
+			String sql = "insert into produto (nome, quantidade, valor) values (?,?,?)";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setString(1, produto.getNome());
+			statement.setDouble(2, produto.getQuantidade());
+			statement.setDouble(3, produto.getValor());
+			statement.execute();
+			connection.commit();
+		} catch (SQLException e) {			
+			try {
+				e.printStackTrace();
+				connection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+		
 	}
 }
--- src/servlet/ProdutoServlet.java
+++ src/servlet/ProdutoServlet.java
@@ -9,9 +9,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import beans.BeanProduto;
+import dao.DaoProduto;
+
 @WebServlet("/produtoServlet")
 public class ProdutoServlet extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
+	private DaoProduto daoProduto = new DaoProduto();
        
     public ProdutoServlet() {
         super();
@@ -23,6 +28,18 @@ public class ProdutoServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		BeanProduto produto = new BeanProduto();
 		
+		String id = request.getParameter("id");
+		
+		produto.setId(!id.isEmpty() ? Long.parseLong(id) : null);
+		produto.setNome(request.getParameter("nome"));
+		produto.setQuantidade(Double.parseDouble(request.getParameter("quant")));
+		produto.setValor(Double.parseDouble(request.getParameter("valor")));
+		
+		daoProduto.salvar(produto);
+		
+		RequestDispatcher view = request.getRequestDispatcher("cadastroProduto.jsp");
+		view.forward(request, response);
 	}
 }
