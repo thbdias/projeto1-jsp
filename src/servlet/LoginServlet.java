67107d6c206d8f819d4f2b0e41d1de971f49e4a8
thbdias <thiagobalbino91@gmail.com>
2019-12-16 07:47:02 -0300

    login e senha vazios

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -14,33 +14,39 @@ import dao.DaoLogin;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	DaoLogin daoLogin = new DaoLogin();
-       
 
-    public LoginServlet() {
-        super();
-    }
+	public LoginServlet() {
+		super();
+	}
 
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doPost(request, response);
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		try {
 			String login = request.getParameter("login");
 			String senha = request.getParameter("senha");
 			
-			if (daoLogin.validarLogin(login, senha)) {
-				RequestDispatcher dispatcher = request.getRequestDispatcher("acessoliberado.jsp");
-				dispatcher.forward(request, response); //faz o redirecionamento
-			}else {
-				RequestDispatcher dispatcher = request.getRequestDispatcher("acessonegado.jsp");
-				dispatcher.forward(request, response); //faz o redirecionamento
+			if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) { //login e senha vazio
+
+				if (daoLogin.validarLogin(login, senha)) {
+					RequestDispatcher dispatcher = request.getRequestDispatcher("acessoliberado.jsp");
+					dispatcher.forward(request, response); // faz o redirecionamento
+				} else {
+					RequestDispatcher dispatcher = request.getRequestDispatcher("acessonegado.jsp");
+					dispatcher.forward(request, response); // faz o redirecionamento
+				}
+			}
+			else {
+				RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+				dispatcher.forward(request, response); // faz o redirecionamento
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
