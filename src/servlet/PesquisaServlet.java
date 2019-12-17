f59f4d6f17f99e5a96bf04ff6e344c359b68b40c
thbdias <thiagobalbino91@gmail.com>
2019-12-17 10:00:41 -0300

    implementacao servlet pesquisa

--- src/servlet/PesquisaServlet.java
+++ src/servlet/PesquisaServlet.java
@@ -1,13 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/PesquisaServlet")
+import dao.DaoUsuario;
+
+@WebServlet("/pesquisaServlet")
 public class PesquisaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -18,7 +22,19 @@ public class PesquisaServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+		try {
+			String descricaoconsulta = request.getParameter("descricaoconsulta");
+			DaoUsuario daoUsuario = new DaoUsuario();
+			
+			if (descricaoconsulta != null) {			
+				RequestDispatcher view = request.getRequestDispatcher("cadastroUsuario.jsp");				
+				request.setAttribute("usuarios", daoUsuario.listar(descricaoconsulta));				
+				view.forward(request, response);				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
