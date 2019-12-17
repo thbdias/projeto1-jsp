d33273cec96d06145c40824b0712e42332b066e8
thbdias <thiagobalbino91@gmail.com>
2019-12-17 11:26:13 -0300

    add campo id categoria no bean de produto

--- src/beans/BeanProduto.java
+++ src/beans/BeanProduto.java
@@ -6,8 +6,19 @@ public class BeanProduto {
 	private String nome;
 	private double quantidade;
 	private double valor;
+	private Long categoria_id;
 	
 	
+	
+	
+	public Long getCategoria_id() {
+		return categoria_id;
+	}
+
+	public void setCategoria_id(Long categoria_id) {
+		this.categoria_id = categoria_id;
+	}
+
 	public Long getId() {
 		return id;
 	}
